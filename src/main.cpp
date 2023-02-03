63e25681b66bd43c9cbad992e5e5591b430c15ec
Steve Goyette <steve_goyette@intuit.com>
2023-02-02 21:37:17 -0800

    Updated to use M5Core libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,17 @@
 #include <Arduino.h>
-#include "M5_ENV.h"
-#include <PubSubClient.h>
 #include <Wire.h>
+
+#include "M5_ENV.h"
+#include "UNIT_MQTT.h"
 #include "WiFi.h"
 
 // WiFi credentials.
-const char* WIFI_SSID = "zzr232";
-const char* WIFI_PASS = "kkrT9aTa1!!";
-const char* mqtt_server = "10.0.1.101"; //mqtt server
+const char *WIFI_SSID = "zzr232";
+const char *WIFI_PASS = "kkrT9aTa1!!";
+const char *mqtt_server = "10.0.1.101";  // mqtt server
 
 WiFiClient espClient;
-PubSubClient client(espClient); //lib required for mqtt
+UNIT_MQTT mqtt;
 QMP6988 qmp6988;
 SHT3X sht30;
 
@@ -31,7 +32,7 @@ void setup() {
   Serial.println("Connecting WIFI...");
   boolean firstTry = true;
   while (WiFi.status() != WL_CONNECTED) {
-    if ( !firstTry ) {
+    if (!firstTry) {
       Serial.println("Retrying....");
     } else {
       firstTry = false;
@@ -53,13 +54,26 @@ void setup() {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 
-  client.setServer(mqtt_server, 1883);//connecting to mqtt server
+  mqtt.Init(&Serial2, 9600, 32, 26);
+  while (!mqtt.isConnectedLAN()) {
+    Serial.print('.');
+  }
+  mqtt.configMQTT(
+      "10.0.1.101",   // host
+      "1883",         // port
+      "M5Stack",      // client id
+      "ha_mqtt",      // user name
+      "IC7610B4by!",  // password
+      "60"            // keepalive
+  );
+  mqtt.configSave();
+  mqtt.startMQTT();
 }
 
 float *getTemperature(float *resultData) {
-
   float temp = 0.0f;
-  while (sht30.get() != 0);
+  while (sht30.get() != 0)
+    ;
   resultData[0] = sht30.cTemp;
   resultData[1] = sht30.humidity;
   return resultData;
@@ -70,30 +84,28 @@ float *getTemperature(float *resultData) {
 ulong timestamp = -10000000L;
 
 void loop() {
-  float tempData[2] = { 0.0f, 0.0f };
+  float tempData[2] = {0.0f, 0.0f};
 
   ulong currentMillis = millis();
-  if ( currentMillis - timestamp >= INTERVAL ) {
-
+  if (currentMillis - timestamp >= INTERVAL) {
     char buffer[128] = "";
-    if (!client.connected()) {
-      Serial.println("Connecting to MQTT...");
-      while (!client.connect("m5stack", "mqtt-ha", "IC7610B4by!" )) {
-        Serial.println("Connection failed....retrying in 5 seconds...");
-        sleep(5);
-      };
-    }
+    // if (!mqtt.isConnectedMQTT()) {
+    //   Serial.println("Connecting to MQTT...");
+    //   while (!mqtt. .connect("m5stack", "mqtt-ha", "IC7610B4by!")) {
+    //     Serial.println("Connection failed....retrying in 5 seconds...");
+    //     sleep(5);
+    //   };
+    // }
     getTemperature(tempData);
     sprintf(buffer, "{\"temperature\": %.2f}", tempData[0]);
     Serial.print("Publishing payload : ");
     Serial.println(buffer);
-    
-    client.publish("office/temperature", buffer);
+
+    mqtt.publish({"office/temperature", buffer, "2"});
     sprintf(buffer, "{\"humidity\": %.2f}", tempData[1]);
-    client.publish("office/humidity", buffer);
+    mqtt.publish({"office/humidity", buffer, "2"});
     Serial.println(buffer);
-    
+
     timestamp = currentMillis;
   }
-  client.loop();
 }
